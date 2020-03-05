b9fac7e9480c4124359bef3936749b104557fe1d
kiminozo <kiminozo.eien@gmail.com>
2020-03-05 14:26:28 +0800

    test

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 const webDB = require('./src/webDB');
 
-webDB.test("Shinchou Yuusha")
+
+const textJson = {
+    dir: [
+        "[Array Stars][Orange][01 - 13][720 P][GB & JAP]",
+        "[BeanSub & FZSD][Taboo_Tattoo][01 - 12][1080 P_Hi10P][MKV]",
+        "[BRB][RWBY][Volume2][1080 P]",
+        "[CASO][Tales_of_Zestiria_the_X][00 - 12][GB][720 p]",
+        "[Comicat][New Game][1 - 12][GB][720 P]",
+        "[DHR & MakariHoshiyume][Ace Attorney][01 - 24][BIG5][720 P][AVC_AAC][MP4]",
+        "[DMG][Magi Sinbad no Bouken][01 - 13][CHS][MP4]",
+        "[DMG][Rewrite][01 - 13 END][720 P_Hi10P][MKV]",
+        "[Hoshioka] planetarian~the reverie of a little planet~[720 p][MKV]",
+        "[JyFanSub][danganronpa 3 Despair][01~11][GB_CN][720 p][MP4]",
+        "[KissSub & FZSD][Gyakuten_Saiban][01 - 24][720 P_Hi10P][MKV]",
+        "[KNA][Momokuri][01 - 13][合集][1280 x720][x264_AAC][BIG5]",
+        "[KTXP][Tenku Senki Shurato][01 - 38][GB_CN][576 p][MP4]",
+        "[Mmch.sub][91 _Days][01 - 12][GB][X264][720 P]",
+        "[Moozzi2] Wake Up, Girls!-Movie 02 - 「Zoku Gekijouban」 - Movie",
+        "[Moozzi2] Wake Up, Girls!-Movie 03 - 「Beyond the Bottom」 - Movie +",
+        "[Pussub][Little Busters!EX][BIG5][x264_AAC][720 P]",
+        "[SHIGURE][konobi][CHS][720 p][Rev]",
+        "[SumiSora][Tanakeda][MKV][1080 p]",
+        "[异域 - 11 番小队][不死之王 Overlord][01 - 13 + SP][BDRIP][1080 P][X264 - 10 bit_",
+        "[Kamigami] Buddha 2 Tezuka Osamu no Buddha - Owarinaki Tabi[BD x264 1 920 x1080 FLAC(5.1 ch) Sub(Chs, Cht, Jap)].mkv",
+        "[KNA][Accel_world_Infinite_Burst][1280 x720][x264_AAC][BIG5].mp4"
+    ]
+}
+
+//webDB.test("Shinchou Yuusha")
+
+let regex = /\[[^\[\]]+\]\S?\[?([^\[\]]+)\]?.+/;
+
+async function test() {
+    for (const name of textJson.dir) {
+        let result = regex.exec(name);
+        if (result) {
+            console.log(result[1]);
+            await webDB.test(result[1])
+        } else {
+            console.log(name + " no match");
+        }
+    }
+}
+
+test();
--- src/webDB.js
+++ src/webDB.js
@@ -1,9 +1,14 @@
 const axios = require('axios');
 
 async function test(name) {
+    name = name.replace("_", " ")
     let text = encodeURI(name)
     let result = await axios.get("https://kitsu.io/api/edge/anime?filter[text]=" + text)
-    console.log(result.data.data[0].attributes.titles.ja_jp);
+    if (result.data.data[0]) {
+        console.log(result.data.data[0].attributes.titles.ja_jp);
+    } else {
+        console.log("no find")
+    }
 }
 
 module.exports = {
